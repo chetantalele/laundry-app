037a36c1a7fbe499a44d479ac8228ff947d683be
chetantalele <chetantalele06@gmail.com>
2025-04-03 19:11:12 +0530

    Updated files with latest changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,11 @@
-c
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Your system's IP address
+    port: 5173,              // Optional: Change the port if needed
+  },
+})
